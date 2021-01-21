161ea05039475465c13dd3ad891bd1a14102f4b1
pr1m0x <kraemer.timo1@gmail.com>
2021-01-21 19:12:16 +0100

    finished lab

--- src/game.js
+++ src/game.js
@@ -1,7 +1,80 @@
+class Treasure {
+  constructor() {
+    this.col = 0;
+    this.row = 0;
+    this.image;
+    this.setRandomPosition();
+  }
+
+  setRandomPosition() {
+    this.col = Math.floor(Math.random() * 10) * 50;
+    this.row = Math.floor(Math.random() * 10) * 50;
+  }
+
+  drawTreasure() {
+    image(this.image, this.col, this.row, 50, 50);
+  }
+
+  foundTreasure(player, treasure) {
+    if (player.col === treasure.col && player.row === treasure.row) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+    this.compass = 'S';
+    this.image;
+    this.imageRight;
+  }
+
+  draw() {
+    this.col = constrain(this.col, 0, 450);
+    this.row = constrain(this.row, 0, 450);
+
+    if (this.compass === 'S') {
+      image(this.image, this.col, this.row, 50, 50);
+    } else if (this.compass === 'E') {
+      image(this.imageLeft, this.col, this.row, 50, 50);
+    } else if (this.compass === 'W') {
+      image(this.imageRight, this.col, this.row, 50, 50);
+    } else {
+      image(this.imageUp, this.col, this.row, 50, 50);
+    }
+  }
+
+  moveUp() {
+    this.row -= 50;
+  }
+  moveDown() {
+    this.row += 50;
+  }
+  moveLeft() {
+    this.col -= 50;
+  }
+  moveRight() {
+    this.col += 50;
+  }
+}
+
 class Game {
+  constructor() {
+    this.player = new Player(50, 50);
+    this.treasure = new Treasure();
+  }
+
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    clear();
+    rect(0, 0, WIDTH, WIDTH);
+
+    for (let i = 0; i < 10; i++) {
+      line(0, SQUARE_SIDE * i, 500, SQUARE_SIDE * i);
+      line(SQUARE_SIDE * i, 0, SQUARE_SIDE * i, 500);
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,9 +2,55 @@ const game = new Game();
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
-  canvas.parent("canvas");
+  canvas.parent('canvas');
+}
+
+function preload() {
+  game.player.image = loadImage('/assets/character-down.png');
+  game.player.imageRight = loadImage('/assets/character-right.png');
+  game.player.imageLeft = loadImage('/assets/character-left.png');
+  game.player.imageUp = loadImage('/assets/character-up.png');
+
+  game.treasure.image = loadImage('/assets/treasure.png');
 }
 
 function draw() {
   game.drawGrid();
+  game.player.draw();
+  game.treasure.drawTreasure();
+}
+
+function keyPressed() {
+  if (keyCode === 38) {
+    game.player.moveUp();
+    game.player.compass = 'N';
+
+    if (game.treasure.foundTreasure(game.player, game.treasure)) {
+      game.treasure.setRandomPosition();
+    }
+  }
+  if (keyCode === 40) {
+    game.player.moveDown();
+    game.player.compass = 'S';
+
+    if (game.treasure.foundTreasure(game.player, game.treasure)) {
+      game.treasure.setRandomPosition();
+    }
+  }
+  if (keyCode === 37) {
+    game.player.moveLeft();
+    game.player.compass = 'E';
+
+    if (game.treasure.foundTreasure(game.player, game.treasure)) {
+      game.treasure.setRandomPosition();
+    }
+  }
+  if (keyCode === 39) {
+    game.player.moveRight();
+    game.player.compass = 'W';
+
+    if (game.treasure.foundTreasure(game.player, game.treasure)) {
+      game.treasure.setRandomPosition();
+    }
+  }
 }
